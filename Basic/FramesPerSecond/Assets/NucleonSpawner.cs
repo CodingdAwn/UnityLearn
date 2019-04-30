251d78d2514a5c8e75e357d2f2eb47182c76c249
gaoxifeng <gaoxifeng@loveasygame.com>
2019-04-30 15:04:49 +0800

    Unity学习 ui 帧数

---
+++ Basic/FramesPerSecond/Assets/NucleonSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour
+{
+    public float timeBetweenSpawns;
+
+    public float spwanDistance;
+
+    public Nucleon[] nucleonsPrefabs;
+
+    private float timeSinceLastSpawn;
+
+    private void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnNucleon();
+        }
+    }
+
+    private void SpawnNucleon()
+    {
+        Nucleon prefab = nucleonsPrefabs[Random.Range(0, nucleonsPrefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+
+        spawn.transform.localPosition = Random.onUnitSphere * spwanDistance;
+    }
+}

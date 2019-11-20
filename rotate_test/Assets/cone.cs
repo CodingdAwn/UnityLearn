171536858e3c169a756dd3e904411250a40efabc
gaoxifeng <gxf_dawn@163.com>
2019-11-20 10:49:22 +0800

    根据两点画椎体

---
+++ rotate_test/Assets/cone.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class cone : MonoBehaviour
+{
+    public int Count;
+    public float R;
+    public float T;
+
+    private void OnDrawGizmos()
+    {
+        Vector3 from = new Vector3(10, 0, 0);
+        Vector3 to = transform.position;
+        Vector3 dir = to - from;
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(from, to);
+        Gizmos.DrawSphere(to, 0.1f);
+        Gizmos.color = Color.red;
+        for (int i = 0; i < Count; i++)
+        {
+
+            Vector3 n = new Vector3();
+            if (dir.x != 0)
+            {
+                n.y = Random.Range(-1f, 1f);
+                n.z = Random.Range(-1f, 1f);
+                n.x = (-dir.z * n.z - dir.y * n.y) / dir.x;
+            }
+            else if (dir.y != 0)
+            {
+                n.x = Random.Range(-1f, 1f);
+                n.z = Random.Range(-1f, 1f);
+                n.y = (-dir.x * n.x - dir.z * n.z) / dir.y;
+            }
+            else if (dir.z != 0)
+            {
+                n.x = Random.Range(-1f, 1f);
+                n.y = Random.Range(-1f, 1f);
+                n.z = (-dir.x * n.x - dir.y * n.y) / dir.z;
+            }
+
+            n = n.normalized * Mathf.Sqrt(Random.Range(0, R));
+            //n = n.normalized * Random.Range(0, R);
+            float t = Random.Range(T, 1f);
+            Gizmos.DrawSphere(from + dir * t + n * t, 0.1f);
+        }
+    }
+}
---
+++ rotate_test/Assets/cone_test.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cone_test : MonoBehaviour
+{
+    public Vector3 pos1;
+    public Vector3 pos2;
+
+    public GameObject Prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        for (int i = 0; i < 10000; ++i)
+        {
+            GameObject go = Object.Instantiate(Prefab);
+            go.transform.position = RandomPos();
+        }
+    }
+
+    private Vector3 RandomPos()
+    {
+        Vector3 dir = (pos2 - pos1).normalized;
+
+        var vector3 = new Vector3(1, 0, 0);
+
+
+        var result1 = Quaternion.AngleAxis(RandomAngle(), Vector3.up) * dir;
+        var result2 = Quaternion.AngleAxis(RandomAngle(), Vector3.forward) * result1;
+        return result2 * RandomLength();
+    }
+
+    private float RandomAngle()
+    {
+        float angle = Random.Range(-30.0f, 30.0f);
+        return angle;
+    }
+
+    private float RandomLength()
+    {
+        float dis = Random.Range(1.0f, (pos2 - pos1).magnitude);
+        return dis;
+    }
+}
